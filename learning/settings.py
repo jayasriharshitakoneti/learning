46c477d600d0607c69b2fcd7bb265a8ba04b14d3
jayasriharshitakoneti <75335955+jayasriharshitakoneti@users.noreply.github.com>
2021-06-18 23:02:31 +0530

    password removed

--- learning/settings.py
+++ learning/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'learning',
         'USER': 'postgres',
-        'PASSWORD':'1234',
+        'PASSWORD':'',
         'HOST': 'localhost'
     }
 }
